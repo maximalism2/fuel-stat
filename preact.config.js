2f6223bbf7701924a9d170a6b8165795e7a3440d
Max Vyz <mvyznyuk@gmail.com>
2018-05-01 19:42:32 +0300

    Change configuration of flow and include firebase from cdn

--- preact.config.js
+++ preact.config.js
@@ -1,6 +1,11 @@
-const preactCliFlow = require("preact-cli-plugin-flow");
+const stripFlowTypeAnnotations = options => {
+  const pluginPathname = `${process.cwd()}/node_modules/babel-plugin-transform-flow-strip-types/lib/index.js`;
+  options.plugins.push(pluginPathname);
+};
 
 export default (config, env, helpers) => {
-  preactCliFlow(config);
-  helpers.setHtmlTemplate(config, "src/index.html");
+  const { rule } = helpers.getLoadersByName(config, "babel-loader")[0];
+  stripFlowTypeAnnotations(rule.options);
+
+  helpers.setHtmlTemplate(config, "./src/index.html");
 };
